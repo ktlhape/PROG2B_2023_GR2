cf59fe737cc8e5cb4ccee37bd6e2df4cda35b1f8
ktlhape <ktlhape@iie.ac.za>
2023-09-04 15:02:39 +0200

    Creating tasks and async methods.

---
+++ AsyncPrograming/AsyncPrograming/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+namespace AsyncPrograming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TaskOptions
+            //Option 1
+            //Task taskOdd = new Task(OddNumbers);
+            //Task taskEven = new Task(EvenNumbers);
+            //taskOdd.Start();
+            //taskEven.Start();
+
+            //Option 2
+            //Task oddTask = Task.Run(() => OddNumbers());
+            //Task evenTask = Task.Run(() => EvenNumbers());
+
+            //Task otherTask = Task.Run(() => {
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        if (i %3 ==0)
+            //        {
+            //            Console.WriteLine(i);
+            //        } 
+            //    }
+
+            //});
+
+            //Option 3
+
+            //Task.Run(() => OddNumbers());
+            //Task.Run(() => EvenNumbers());
+            //Task.Run(() =>
+            //{
+            //    for (int i = 1; i <= 10; i++)
+            //    {
+            //        if (i % 3 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //});
+
+            #endregion
+
+            Task taskOdd = OddNumbers();
+            Task taskEven = Task.Run(() => EvenNumbers());
+            //Task.WaitAll(taskOdd);
+            DisplaySum();
+
+            Console.Read();
+        }
+        static void Test()
+        {
+
+        }
+
+        static async Task OddNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(2000);
+                if (i %2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static async Task EvenNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(2000);
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static int GetSum()
+        {
+            Thread.Sleep(5000);
+            int sum = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        static async void DisplaySum()
+        {
+            Task<int> taskSum = new Task<int>(GetSum);
+            taskSum.Start();
+            Console.WriteLine("Busy calculating the sum...");
+            int sum = await taskSum;
+            Console.WriteLine($"Sum: {sum}");
+        }
+      
+    }
+}
---
+++ MultithreadingWPF/MultithreadingWPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace MultithreadingWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void EvenNumbers()
+        {
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(2000);
+                if (i % 2 == 0)
+                {
+                    lstEven.Items.Add(i);
+                }
+            }
+        }
+        public void OddNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(2000);
+                if (i % 2 != 0)
+                {
+                    lstOdd.Items.Add(i);
+                }
+            }
+        }
+
+        private void btnEven_Click(object sender, RoutedEventArgs e)
+        {
+            //offload the method from the main thread.
+            Thread thEven = new Thread(EvenNumbers);
+            thEven.Start();
+        }
+
+        private void btnOdd_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thOdd = new Thread(OddNumbers);
+            thOdd.Start();
+            
+        }
+    }
+}
