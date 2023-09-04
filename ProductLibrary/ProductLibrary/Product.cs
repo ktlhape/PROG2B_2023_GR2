370c0eddd6d2aa6207ce4d0a335b5ec17037018f
ktlhape <ktlhape@iie.ac.za>
2023-09-04 16:02:00 +0200

    Creating Class Libraries

---
+++ ProductLibrary/ProductLibrary/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductLibrary
+{
+    /// <summary>
+    /// Product class to store product information
+    /// </summary>
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        private static List<Product> productList = new List<Product>();
+        /// <summary>
+        /// Create a Product object with the Code, Description and the Price
+        /// </summary>
+        /// <param name="code">Product Code</param>
+        /// <param name="desc">Product Description</param>
+        /// <param name="price">Product Price</param>
+        public Product(string code, string desc, double price)
+        {
+            Code = code;
+            Description = desc;
+            Price = price;
+        }
+        /// <summary>
+        /// Add Product to a collection
+        /// </summary>
+        /// <param name="p">Product to be added</param>
+        public static void AddProduct(Product p)
+        {
+            productList.Add(p);
+        }
+        /// <summary>
+        /// Get all the products from a collection
+        /// </summary>
+        /// <returns>All products</returns>
+        public static List<Product> GetProducts() => productList;
+        /// <summary>
+        /// Get product information or details
+        /// </summary>
+        /// <returns>Product details</returns>
+        public override string ToString() =>
+            $"({Code}) {Description} - {Price.ToString("C2")}";
+        
+    }
+}
---
+++ TestingProductLibrary/TestingProductLibrary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductLibrary;
+
+namespace TestingProductLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product.AddProduct(new Product("P1", "Coke", 18.00));
+            Product.AddProduct(new Product("P2", "Fanta", 19.00));
+            Product.AddProduct(new Product("P3", "Milk", 27.00));
+            Product.AddProduct(new Product("P4", "Maize", 32.99));
+            Product.AddProduct(new Product("P5", "Sprite", 19.50));
+            Product.AddProduct(new Product("P6", "Mango", 9.50));
+            Product.AddProduct(new Product("P7", "Berries", 53.89));
+
+            foreach (Product p in Product.GetProducts())
+            {
+                Console.WriteLine($"{p.ToString()}");
+            }
+            Console.ReadLine();
+           
+
+
+
+        }
+    }
+}
